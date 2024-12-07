4527993e60b3188e1cac1e239478363e0452236c
mo2397 <166650104+mo2397@users.noreply.github.com>
2024-12-07 15:46:21 -0700

    n

--- zDynamic Portfolio/portfolio_2.js
+++ zDynamic Portfolio/portfolio_2.js
@@ -27,10 +27,48 @@ function findProjectInJSON(projects){
     }
 }
 
+// function buildPage(project){
+//     console.log(project);
+//     document.getElementById("project").innerHTML += `<h1>${project.name}</h1>`;
+//     console.log("hi " + project.name);
+// }
+
+// const displayedImage = document.getElementsByClassName('displayed-img');
+const displayedImage = document.querySelector('.displayed-img');
 function buildPage(project){
     console.log(project);
     document.getElementById("project").innerHTML += `<h1>${project.name}</h1>`;
-    console.log("hi");
+    console.log("hi " + project.name);
+    const thumbBar = document.querySelector('.thumb-bar');
+
+    /* Looping through images */
+    for (let i = 0; i < project.images.length; i++){
+        const currentPic = project.images[i];
+        const newImage = document.createElement('img');
+        newImage.setAttribute('src', currentPic.src);
+        newImage.setAttribute('alt', currentPic.alt);
+        thumbBar.appendChild(newImage);
+
+        newImage.addEventListener('click', function(e) {
+            // alert(displayedImage.alt);
+            displayedImage.src = e.target.src;
+            displayedImage.alt = e.target.alt;
+        });
+    }
+
+
+    // const newImage = document.createElement('img');
+    // newImage.setAttribute('src', xxx);
+    // newImage.setAttribute('alt', xxx);
+    // thumbBar.appendChild(newImage);
+
+    
 }
 
 console.log("portfolio.js is loaded");
+
+const slideshowCarasole = document.getElementById("slideshow");
+
+const carouselContainer = document.createElement('div');
+
+
